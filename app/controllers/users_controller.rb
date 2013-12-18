2c720c23d6ce15b5718cce6fae062cc148c8295d
Julie Pranger <juliepranger@gmail.com>
2013-12-17 16:10:26 -0800

    user agent, screw you

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,30 @@
 class UsersController < ApplicationController
 
 	def index
+		@users = User.all
+		@user = current_user
+		unless current_user.admin == "true"
+		redirect_to root_path, notice: "Uh oh, you don't have permission to view that!"
+		end #still need to create an admin
 	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+	end
+
+	def show
+		@user = User.find(params[:id])
+		unless current_user == @user || current_user.admin
+			redirect_to users_path, notice: "You can only edit your own profile."
+			#privacy issues
+	end
+
+	def destroy
+		User.find(params[:id]).destroy
+		redirect_to users_url
+	end
+
 end
