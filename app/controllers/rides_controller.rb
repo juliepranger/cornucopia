5bd8f270a95740761a4f57fb2826c75aaf90b480
Julie Pranger <juliepranger@gmail.com>
2013-12-18 10:38:09 -0800

    updated styling on recipes show

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -3,11 +3,11 @@ class RidesController < ApplicationController
 	skip_before_action :require_login, only:[:show, :index]
 	respond_to :html, :json
 
-def new
-	@ride = Ride.new
-end
+	def new
+		@ride = Ride.new
+	end
 
-def create_ride_access
+	def create_ride_access
 		if current_user == :userid
 			redirect_to :action => 'show', :id =>@party._id
 		else
@@ -15,20 +15,25 @@ def create_ride_access
 		end
 	end
 
-def create
-	@party = Party.find(params[:id])
-	logger.info @party.inspect
-	@ride = @party.rides.new(name: params[:name], lat: params[:lat], lng: params[:lng], date: params[:date], need_ride_location: params[:location])
-    if @ride.save!
-      render :status => :ok, :json => { status: 'SUCCESS', url: party_path(@party) }
-  	else
-      render :status => 422, :json => { status: 'FAILED' }
- 		end
-end
+	def create
+		@party = Party.find(params[:id])
+		logger.info @party.inspect
+		@ride = @party.rides.new(name: params[:name], lat: params[:lat], lng: params[:lng], date: params[:date], need_ride_location: params[:location])
+	    if @ride.save!
+	      render :status => :ok, :json => { status: 'SUCCESS', url: party_path(@party) }
+	  	else
+	      render :status => 422, :json => { status: 'FAILED' }
+	 		end
+	end
 
-def show
-	
-end
+	def show
+		
+	end
+
+	def destroy
+		@ride = Party.ride.find(params[:id]).destroy
+		redirect_to party_path
+	end
 
 private
 
