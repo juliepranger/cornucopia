e41a3279f528f96470ed635d2080039a06c9308e
Julie Pranger <juliepranger@gmail.com>
2014-01-07 16:52:56 -0800

    before heroku

--- config/initializers/omniauth.rb
+++
@@ -1,6 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'],
-  scope: 'email, user_location', image_size:{type: 'small'}, info_fields: 'name, first_name, last_name, user_location, image'
-end
